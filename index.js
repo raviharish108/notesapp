39aaa80aede5799f2a9c11a6d385b5e5446e14a4
RaviHarish <raviharish108@gmail.com>
2023-03-06 23:20:58 +0800

    i want to send email

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import userroutes from "./routes/user.js"
 import notesroute from "./routes/notes.js"
+import { sendTxt } from "./sendtxt.js";
 dotenv.config();
 const PORT= 5000;
 const app=express();
@@ -15,7 +16,15 @@ app.get("/",(req,res)=>{
 })
 app.use("/api/user",userroutes);
 app.use("/api/notes",notesroute);
-
+app.post("/api/send/txt",async(req,res)=>{
+    try{
+         const{txt,from_email}=req.body;
+         await sendTxt(txt,from_email);
+         return res.status(200).json({msg:"successfully message send"})
+    }catch(err){
+        return res.status(400).json({msg:err.message})
+    }
+})
 app.listen(PORT, async() => {
     await connect();
     await console.log('Server is running on port', PORT)
---
+++ sendtxt.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer"
+
+export const sendTxt=async(txt,from_email)=>{
+    const transporter = nodemailer.createTransport({
+       service:"gmail",
+        auth: {
+            user:process.env.username, 
+            pass:process.env.password ,  
+        }
+    });
+    const message = {
+        from: from_email,
+        to:"raviharish108@gmail.com",
+        subject: txt,
+        html:`
+           <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the Ravi App.</h2>
+            <p>${txt}</p>
+            <div style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</div>
+            </div>`
+       
+    };
+    await transporter.sendMail(message, function(error, info){ 
+        if (error) { 
+          console.log(error); 
+        } else { 
+          console.log('Email sent: ' + info.response); 
+        } 
+      });
+
+
+}
